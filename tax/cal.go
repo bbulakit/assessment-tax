a90667e73155e518090d9f4e191349a383cb9f04
bbulakit <b.bulakit@hotmail.com>
2024-04-20 22:59:36 +0700

    implemented TaxRate

--- tax/cal.go
+++ tax/cal.go
@@ -5,5 +5,14 @@ func TaxCalculate(tax Tax) float64 {
 }
 
 func TaxRate(totalIncome float64) float64 {
-	return 0
+	if totalIncome <= 150000 {
+		return 0.00
+	} else if totalIncome <= 500000 {
+		return 0.10
+	} else if totalIncome <= 1000000 {
+		return 0.15
+	} else if totalIncome <= 2000000 {
+		return 0.20
+	}
+	return 0.35
 }
