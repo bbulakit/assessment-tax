c7811b3f84d3bfd9e4929ce52ef6c9214aefeff1
bbulakit <b.bulakit@hotmail.com>
2024-04-20 20:57:03 +0700

    Added Graceful shutdown at main

--- main.go
+++ main.go
@@ -1,8 +1,13 @@
 package main
 
 import (
-	"github.com/labstack/echo/v4"
+	"context"
 	"net/http"
+	"os"
+	"os/signal"
+	"time"
+
+	"github.com/labstack/echo/v4"
 )
 
 func main() {
@@ -10,5 +15,19 @@ func main() {
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, Go Bootcamp!")
 	})
-	e.Logger.Fatal(e.Start(":1323"))
+
+	go func() {
+		if err := e.Start(":1323"); err != nil && err != http.ErrServerClosed { // Start server
+			e.Logger.Fatal("shutting down the server")
+		}
+	}()
+
+	shutdown := make(chan os.Signal, 1)
+	signal.Notify(shutdown, os.Interrupt)
+	<-shutdown
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	if err := e.Shutdown(ctx); err != nil {
+		e.Logger.Fatal(err)
+	}
 }
