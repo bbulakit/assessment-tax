7cb276da760c650d461f513dedf96bdf4efb0239
bbulakit <b.bulakit@hotmail.com>
2024-04-20 22:57:33 +0700

    Added simple Tax Testing

---
+++ tax/cal.go
@@ -0,0 +1,9 @@
+package tax
+
+func TaxCalculate(tax Tax) float64 {
+	return 0
+}
+
+func TaxRate(totalIncome float64) float64 {
+	return 0
+}
---
+++ tax/cal_test.go
@@ -0,0 +1,59 @@
+package tax
+
+import "testing"
+
+func TestTaxCalulate(t *testing.T) {
+	test := struct {
+		tax  Tax
+		want float64
+	}{
+		tax: Tax{
+			TotalIncome:    500_000,
+			WithHoldingTax: 0,
+			Allowances: []Allowance{{
+				AllowanceType: "donation",
+				Amount:        0,
+			}},
+		},
+		want: 29_000,
+	}
+
+	got := TaxCalculate(test.tax)
+
+	if got != test.want {
+		t.Errorf("Got= %f; want %f", got, test.want)
+	}
+}
+
+func TestTaxRate(t *testing.T) {
+	tests := []struct {
+		name        string
+		totalIncome float64
+		want        float64
+	}{
+		{name: "Income=0 then TaxRate=0", totalIncome: 0, want: 0.00},
+		{"Income<=150,000 then TaxRate=0", 150_000, 0.00},
+		{"Income=150,001 then TaxRate=10%", 150_001, 0.10},
+		{"Income<=500,000 then TaxRate=10%", 500_000, 0.10},
+		{"Income=500,001 then TaxRate=15%", 500_001, 0.15},
+		{"Income<=1,000,000 then TaxRate=15%", 1_000_000, 0.15},
+		{"Income=1,000,001 then TaxRate=20%", 1_000_001, 0.20},
+		{"Income<=2,000,000 then TaxRate=20%", 2_000_000, 0.20},
+		{"Income>2,000,000 then TaxRate=35%", 2_000_001, 0.35},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			//Arrange
+
+			//Act
+			got := TaxRate(test.totalIncome)
+
+			//Assert
+			if test.want != got {
+				t.Errorf("TaxRate(%f) = %f; want %f", test.totalIncome, got, test.want)
+			}
+		})
+	}
+
+}
